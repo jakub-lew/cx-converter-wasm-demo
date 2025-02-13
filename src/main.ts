46d7855f45f467b88ab4106ec068c47bc39b67e4
jakubcreoox <113886722+jakubcreoox@users.noreply.github.com>
2025-02-13 16:16:09 +0100

    feat: polished code with use of vite

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import './style.css'
+import { ifc2gltf } from "@creooxag/cx-converter"
+import { Viewer, GLTFLoaderPlugin, TreeViewPlugin } from "@xeokit/xeokit-sdk";
+const viewer = new Viewer({
+  canvasId: "myCanvas"
+});
+const treeView = new TreeViewPlugin(viewer, {
+  containerElement: document.getElementById("myTreeView") as HTMLElement
+});
+const gltfLoader = new GLTFLoaderPlugin(viewer);
+
+async function fileInputChanged() {
+  let fileInput = document.getElementById("finput") as HTMLInputElement;
+  if (fileInput!.files!.length == 0) return console.log("No files selected!");
+  const file = fileInput!.files![0];
+  var reader = await new FileReader();
+  reader.onload = async () => {
+    var data = new Uint8Array(reader.result as ArrayBuffer);
+    try {
+      const ext = file.name.split('.').pop();
+      if (ext == "ifc" || ext == "ifczip") {
+        const { gltf, metaData } = await ifc2gltf(data as unknown as string);
+        const model = await gltfLoader.load({
+          id: "myModel",
+          gltf: gltf,
+          //@ts-ignore
+          metaModelJSON: metaData
+          // autoMetaModel: true
+        });
+        model.on("loaded", () => {
+          viewer.cameraFlight.flyTo();
+          //the following, together with autoMetaModel: true works but flat ID list in treeview is visible
+          // viewer.metaScene.createMetaModel("metaModel", metaData);
+        });
+      }
+    }
+    catch (e) {
+      console.log("loadModel failed: " + e)
+    }
+  };
+  reader.readAsArrayBuffer(file);
+}
+
+document!.getElementById("finput")!.addEventListener("change", fileInputChanged);
